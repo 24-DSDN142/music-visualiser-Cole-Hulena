0467ba7f60133565f6b3c5b323a4370f8c2c9678
Cole-Hulena <96960962+Cole-Hulena@users.noreply.github.com>
2024-08-21 10:54:44 +1200

    Upside Down Added

--- music_starter.js
+++ music_starter.js
@@ -10,7 +10,7 @@ const frames = [];
 const bananaFrames = [];
 const xPos = [1152, 1080, 1008, 936, 864, 792, 720, 648, 576, 504, 432, 360, 288, 216, 144, 72, 0, -72, -144];
 const invertedxPos= [-138, -210, -282, -354, -426, -498, -570, -642, -714, -786, -858, -930, -1002, -1074, -1146, -1218, -1290, -1362, -1434, -1506];
-
+let myFont;
 
 
 const centerX = 720;
@@ -49,16 +49,14 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     bananaFrames.push(loadImage('assets/B7.png'));
     bananaFrames.push(loadImage('assets/B8.png'));
 
-    banana = loadImage('assets/banana.png');
-    // hat = loadImage('assets/hat.png');
+    myFont = loadFont('assets/ScriptMTBold.ttf');
     
     firstrun = false;
   }
   background(8, 14, 75, 50)//background colour 
-  textFont('Helvetica'); // please use CSS safe fonts
+  textFont(myFont); // please use CSS safe fonts
   //rectMode(CENTER)
-  textSize(24);
-
+  
   if(counter > 4250 && counter < 4550){
     inverted = true;
   } else if(counter > 7600 && counter < 7950){
@@ -76,22 +74,48 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     }
   }
 
-
-
-  drawVocal(vocal);
+  drawStarVocal(40, bass);
+  drawStarBass(400, vocal);
+  drawStarOther(780, other);
 
   //draw bananas
-  otherSound(drum, counter);
+  drawDrum(drum, counter);
 
   //draw monkey
   if(currentFrame <= frames.length - 1){
     drawMonkey(currentFrame);
   }
 
+  if(counter > 4250 && counter < 4350 || counter > 9350 && counter < 9550){
+    push();
+    scale(-1,-1);
+    textAlign(CENTER);
+    textFont(myFont);
+    textSize(103);
+    fill(238, 220, 130);
+    text("Upside down", -centerX, -centerY);
+    textSize(100);
+    fill(255,13,0);
+    text("Upside down", -centerX, -centerY);
+    pop();
+  }else if(counter > 7600 && counter < 7950){
+    push();
+    textAlign(CENTER);
+    textFont(myFont);
+    textSize(103);
+    fill(238, 220, 130);
+    text("Upside down", centerX, centerY);
+    textSize(100);
+    fill(255,13,0);
+    text("Upside down", centerX, centerY);
+    pop();
+  }
+   
+
 
 }
 
-function otherSound(input, count){
+function drawDrum(input, count){
   //works out what image
 
   if(input > 20){
@@ -129,22 +153,18 @@ function drawMonkey(frame){
 
 // 820 y
 
-function drawVocal(input){
-  if(input >= 20 && input <= 80){
-  input = map(input, 20, 80, 1, 30);
+function drawStarVocal(y , input){
+  let size;
+  if(input >= 20 && input <= 50){
+  size = map(input, 20, 50, 1, 10);
+  }else if(input >= 40 && input <= 80){
+    size = map(input, 40, 80, 10, 40);
   }else if(input < 20 ){
-    input = 1;
+    size = 1;
   }else{
-    input = 30;
+    size = 40;
   }
 
-  drawStar1(20, input, 0);
-  drawStar2(400, input, 2);
-  drawStar3(780, input, 1);
-}
-
-
-function drawStar1(y , size){
   //increase x
   if(star1X <= 1440){
     star1X += 2;
@@ -155,11 +175,23 @@ function drawStar1(y , size){
 
   //draw star
   push();
+  ellipseMode(CENTER);
   fill(255);
   ellipse(star1X, y, size, size);
+  fill(50);
+  ellipse(star1X, y, size/2, size/2);
   pop();
 }
-function drawStar2(y , size){
+function drawStarBass(y , input){
+  let size;
+  if(input >= 20 && input <= 80){
+  size = map(input, 20, 80, 1, 30);
+  }else if(input < 20 ){
+    size = 1;
+  }else{
+    size = 30;
+  }
+
   //increase x
   if(star2X <= 1440){
     star2X += 3;
@@ -174,7 +206,16 @@ function drawStar2(y , size){
   ellipse(star2X, y, size, size);
   pop();
 }
-function drawStar3(y , size){
+function drawStarOther(y , input){
+  let size;
+  if(input >= 20 && input <= 80){
+  size = map(input, 20, 80, 1, 30);
+  }else if(input < 20 ){
+    size = 1;
+  }else{
+    size = 30;
+  }
+
   //increase x
   if(star3X <= 1440){
     star3X += 4;
